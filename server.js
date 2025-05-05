3b4d7f85a002653261dd90890996d7cd062b6623
shailendr pawar <shailendrapawar980@gmail.com>
2025-05-05 21:53:31 +0530

    addeD: minor chnages

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from "express"
 
-import { app,myHttpServer } from "./socket/socket.js";
+import { app, myHttpServer } from "./socket/socket.js";
 import { configDotenv } from "dotenv";
 configDotenv()
 import cors from "cors"
 import connectDb from "./configs/dbConnect.js";
-connectDb()
+// connectDb()
 import cookieParser from "cookie-parser";
 
 
@@ -21,19 +21,19 @@ import messageRouter from "./routes/messageRoutes.js";
 //=========root middlewares========================
 
 app.use(cors({
-    origin:["http://localhost:5173"],
-    credentials:true,
+    origin: ["http://localhost:5173"],
+    credentials: true,
 }))
 app.use(express.json())
 app.use(cookieParser())
 
 
 
-app.use("/auth",authRouter)
-app.use("/trip",tripRouter)
-app.use("/message",messageRouter)
+app.use("/auth", authRouter)
+app.use("/trip", tripRouter)
+app.use("/message", messageRouter)
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("wokring server")
 })
 
@@ -42,7 +42,10 @@ app.get("/",(req,res)=>{
 
 
 //=======server port connection====================
-const PORT=process.env.PORT||5000
-myHttpServer.listen(PORT,()=>{
-    console.log(`4http server listening at ${PORT}`)
-})
+
+connectDb().then((res) => {
+    const PORT = process.env.PORT || 5000
+    myHttpServer.listen(PORT, () => {
+        console.log(`http server listening at ${PORT}`)
+    })
+}).catch((err)=>console.log(err))
